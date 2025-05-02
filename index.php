452a3b0157dcf8bbc16c417ae056405dc004d706
Kukwac Adiebo <kukwac.adiebo@stu.ocsb.ca>
2025-05-02 15:22:49 +0000

    Testing

--- index.php
+++ index.php
@@ -1,38 +1,23 @@
 <!DOCTYPE html>
 <html lang="en-ca">
   <head>
-    <!-- Metadata -->
     <meta charset="utf-8">
-    <meta name="description" content="Speed of Light">
-    <meta name="keywords" content="immaculata, icd2o">
-    <meta name="author" content="Kukwac">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- Code for the favicon -->
-    <link rel="apple-touch-icon" sizes="180x180" href="./Favicons/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="./Favicons/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="./Favicons/favicon-16x16.png">
-    <link rel="manifest" href="./Favicons/site.webmanifest">
-    <!-- Link to CSS file -->
-    <link rel="stylesheet" href="./css/style.css">
-    <!-- Title -->
     <title>Speed of Light</title>
   </head>
   <body>
-		<!-- php echo to print the html to the page -->
-    <?php 
-			echo "<h1>Speed of Light</h1>";
-			echo "<h3>This program will calculate the Speed of Light in different Mediums.</h3>";
-		?>
-		<!-- form to get the indexes of refraction from the user -->
-    <form action="./results.php" method="post" target="results">
-      <label for="speedLightDiamond">Speed of Light in Diamond (m/s)</label>
-      <input type="float" id="indexRefDiamond" placeholder="Index of Refraction (n)..." name="IndexRefDiamond"><br><br>
-      <label for="speedLightWater">Speed of Light in Water (m/s)</label>
-      <input type="float" id="indexRefWater" placeholder="Index of Refraction (n)..." name="indexRefWater"><br><br>
+    <h1>Speed of Light</h1>
+    <h3>This program will calculate the Speed of Light in different Mediums.</h3>
+
+    <form action="results.php" method="post" target="results">
+      <label for="indexRefDiamond">Index of Refraction (Diamond):</label>
+      <input type="text" id="indexRefDiamond" name="indexRefDiamond" required><br><br>
+
+      <label for="indexRefWater">Index of Refraction (Water):</label>
+      <input type="text" id="indexRefWater" name="indexRefWater" required><br><br>
+
       <input type="submit" value="Calculate Speed">
     </form>
 
-			<!-- iframe for the results to show on the web page. -->
-			<iframe id="results" name="results" style="width:100%; height:150px;"></iframe>
-	</body>
+    <iframe id="results" name="results" style="width:100%; height:150px;"></iframe>
+  </body>
 </html>
--- results.php
+++ results.php
@@ -1,18 +1,18 @@
 <?php
-	// get the indexes of refraction from the textfields
-	$indexDiamond = $_POST['indexRefDiamond'];
-	$indexWater = $_POST['indexRefWater'];
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $indexDiamond = floatval($_POST['indexRefDiamond']);
+    $indexWater = floatval($_POST['indexRefWater']);
 
-	// calculate the speed of light in diamond and water
-	$speedLightAir = 299792458; // speed of light in air in m/s
-	$speedLightDiamond = $speedLightAir / $indexDiamond;
-	$speedLightWater = $speedLightAir / $indexWater;
-	
-	// round the speed of light to 2 decimal places
-	$speedLightDiamond = sprintf("%.2f", $speedLightDiamond);
-	$speedLightWater = sprintf("%.2f", $speedLightWater);
+    $speedLightAir = 299792458;
 
-	echo "<h3>Results:</h3>";
+    $speedLightDiamond = $speedLightAir / $indexDiamond;
+    $speedLightWater = $speedLightAir / $indexWater;
+
+    $speedLightDiamond = number_format($speedLightDiamond, 2);
+    $speedLightWater = number_format($speedLightWater, 2);
+
+    echo "<h3>Results:</h3>";
     echo "The speed of light in diamond is {$speedLightDiamond} m/s.<br>";
     echo "The speed of light in water is {$speedLightWater} m/s.";
+}
 ?>
